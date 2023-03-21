34a2ab9837093e36f4c4733db893bfb816ff78fa
VitorMendesco <vitormendesco.js@gmail.com>
2023-03-20 21:00:15 -0300

    build📦(package): setting up rollup

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import typescript from 'rollup-plugin-typescript2'
+
+const packageJson = require('./package.json')
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    terser(),
+  ],
+}
