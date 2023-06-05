a1bff046c8c922e3e9467338639a7c978bc8de24
VitorMendesco <vitormendesco.js@gmail.com>
2023-06-05 00:42:20 -0300

    feat(✨storybook): adding storybook to project

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,6 +1,6 @@
 import type { ButtonType } from './Button.types'
 
-const Button: ButtonType = ({ buttonRef, children, ...props }) => {
+const Button: ButtonType = ({ buttonRef, children, variant, ...props }) => {
   return (
     <button ref={buttonRef} {...props}>
       {children}
--- src/components/button/Button.types.ts
+++ src/components/button/Button.types.ts
@@ -2,6 +2,7 @@ import { ButtonHTMLAttributes, FC, Ref } from 'react'
 
 export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   buttonRef?: Ref<HTMLButtonElement>
+  variant?: 'primary' | 'secondary' | 'tertiary'
 }
 
 export type ButtonType = FC<ButtonProps>
