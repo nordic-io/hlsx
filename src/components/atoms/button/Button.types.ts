29954ecc732cca487e9b322d002cb18b64e42a7d
VitorMendesco <vitormendesco.js@gmail.com>
2023-06-15 23:07:28 -0300

    feat(✨Button): adding ref as prop

--- src/components/atoms/button/Button.types.ts
+++ src/components/atoms/button/Button.types.ts
@@ -1,10 +1,9 @@
-import { FC } from 'react'
+import { FC, Ref } from 'react'
 
 import { ButtonProps as ButtonPropsUI } from 'baseui/button'
 
-export type ButtonProps = ButtonPropsUI
-// export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-//   buttonRef?: Ref<HTMLButtonElement>
-// }
+export type ButtonProps = ButtonPropsUI & {
+  Ref?: Ref<HTMLButtonElement>
+}
 
 export type ButtonType = FC<ButtonProps>
