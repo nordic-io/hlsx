2b52cdd565c97853fcbff1bd594dc49d87bfaf3f
VitorMendesco <vitormendesco.js@gmail.com>
2023-07-23 14:30:29 -0300

    feat(👷build): moving rollup config to vite config file and updating path aliases

--- src/dist.ts
+++
@@ -1,5 +0,0 @@
-export * from './components'
-export * from './utils'
-
-export * from 'baseui/tokens'
-export * from 'baseui/typography'
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export * from './dist'
+export * from './components'
+export * from './providers'
+export * from './utils'
+
+export * from 'baseui/tokens'
+export * from 'baseui/typography'
