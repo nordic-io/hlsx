0b1933f024fda4f7e06ab706d16ed64df883f698
VitorMendesco <vitormendesco.js@gmail.com>
2023-07-23 14:54:40 -0300

    feat(✨helpers): adding useStyles custom hook to generate css stylesheets

---
+++ src/utils/helpers/useStyles.ts
@@ -0,0 +1,14 @@
+import { Theme, useStyletron } from 'baseui'
+
+import type { CSSProperties } from 'react'
+
+const useStyles = <T extends Record<string, CSSProperties>>(
+  styles: (theme: Theme) => T
+) => {
+  const [, theme] = useStyletron()
+  const classes = styles(theme)
+  return {
+    ...classes
+  }
+}
+export { useStyles }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,1 @@
-export * from './wrappers'
+export * from './helpers'
